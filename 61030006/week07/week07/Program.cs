063f48875d74b39bd30a6e73a2af8918d6cf1a1b
kkkanitar <khanita03272@gmail.com>
2019-11-24 22:17:39 +0700

    gfd

--- 61030006/week07/week07/Program.cs
+++ 61030006/week07/week07/Program.cs
@@ -7,44 +7,14 @@ using System.Threading.Tasks;
 namespace week07
 {
     class Program
-        {
+    {
         static void Main(string[] args)
         {
-            Person p = new Person();
+            Person p = new Professor();
             p.SayHi();
-            Student s = new Student();
-            s.SayHi();
-            Person sp = new Student();
-            sp.SayHi();
-            Teacher t = new Teacher();
-            t.SayHi();
-            Person tp = new Teacher();
-            tp.SayHi();
             Console.ReadKey();
         }
-    }
-    class Person
-    {
-        public virtual void SayHi()
-        {
-            Console.WriteLine("Hi I'm a person");
-        }
-    }
 
-    class Student : Person
-    {
-        public override void SayHi()
-        {
-            Console.WriteLine("Hi I'm a student");
-        }
-    }
-
-    class Teacher : Person
-    {
-        public new void SayHi()
-        {
-            Console.WriteLine("Hi I'm a teacher");
-        }
     }
 }
     
