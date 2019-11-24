326ab1e818e58931d0491768fc24f2e99d02b1da
kkkanitar <khanita03272@gmail.com>
2019-11-24 22:19:28 +0700

    bvc

--- 61030006/week07/week07/Program.cs
+++ 61030006/week07/week07/Program.cs
@@ -10,8 +10,8 @@ namespace week07
     {
         static void Main(string[] args)
         {
-            Student p = new Professor();
-            p.SayHi();
+            Teacher t = new Person();
+            t.SayHi();
             Console.ReadKey();
         }
 
