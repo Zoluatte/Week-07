1af0935028165a54335b60c00dc87ca9f6deb6b1
maethapon1999 <61030045@kmitl.ac.th>
2019-12-16 15:01:19 +0700

    ฟกก

--- 61030045/Week-07/Week-07/Program.cs
+++ 61030045/Week-07/Week-07/Program.cs
@@ -63,3 +63,24 @@ namespace Week_07
         }
     }
 }
+class Teacher
+{
+    // constructor (for initial private/protected variables)
+    public Teacher(string name, float billingRate)
+    {
+        this.name = name;
+        this.billingRate = billingRate;
+    }
+    // figure out the charge based on teacher's rate
+    public float CalculateCharge(float hours)
+    {
+        return (hours * billingRate);
+    }
+    // return the name of this type
+    public string TypeName()
+    {
+        return ("Teacher");
+    }
+    private string name;
+    protected float billingRate;
+}
